18b18fc0c23f394de76a18867e5567276a954dd4
Tiberius <mercytiberius@yahoo.com>
2021-03-23 01:46:49 +0300

    Add change admin password function

--- CarRentalSystem/Variables.h
+++ CarRentalSystem/Variables.h
@@ -10,20 +10,21 @@ std::string dashes = "--------------------------------------------";
 
 std::ifstream admin_name("adminusername.txt");
 
-std::ifstream admin_password("adminpassword.txt");
+std::ifstream admin_password("adminpassword.txt", std::ios::app);
 
-
+std::ofstream admin_password_copy("newadminpassword.txt", std::ios::app);
 
 //Normal User DB
 
 std::ofstream normal_user_name("username.txt", std::ios::app);
 
 std::ofstream normal_user_password("userpassword.txt", std::ios::app);
-std::ofstream normal_user_deposit("userpassword.txt", std::ios::app);
+std::ofstream normal_user_deposit("userdeposit.txt", std::ios::app);
 
 std::ifstream normal_user_name_copy("username.txt", std::ios::app);
 
 std::ifstream normal_user_password_copy("userpassword.txt", std::ios::app);
+std::ifstream normal_user_deposit_copy("userdeposit.txt", std::ios::app);
 
 
 std::string confirm_username = "";
